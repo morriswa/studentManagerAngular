1699417d1f12477d820b4996c339b8793daccb40
morriswa <william@morriswa.org>
2022-06-30 12:56:58 -0500

    Finally hit that damn endpoint! Hello World :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResthttpComponent } from './resthttp/resthttp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResthttpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ResthttpComponent]
 })
 export class AppModule { }
