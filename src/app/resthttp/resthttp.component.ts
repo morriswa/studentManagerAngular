9498ff2fa742e002c902b5d3b2a05ec4e8cf54db
morriswa <william@morriswa.org>
2022-07-01 14:56:46 -0500

    new repogit add .

--- src/app/resthttp/resthttp.component.ts
+++ src/app/resthttp/resthttp.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Response } from '../interface/response';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { LoginRequest } from '../interface/login-request';
 
 @Component({
   selector: 'app-resthttp',
@@ -14,31 +13,46 @@ export class ResthttpComponent {
   readonly HTTP_URL: string = 'http://localhost:8080/';
   message: string = "no data yet";
 
-  public loginForm: FormGroup; 
+  loginForm: FormGroup;
+  studentForm: FormGroup;
 
-  constructor(private http: HttpClient, private fb: FormBuilder) 
+  constructor(private http: HttpClient, private fb: FormBuilder ) 
   {
     this.loginForm = this.fb.group({
-      username: "",
-      password: ""
-    });
-   }
+      "username" : "",
+      "password" : "",
+      "new_password" : ""
+    })
 
+    this.studentForm = this.fb.group({
+      "nickname" : "",
+      "year" : "",
+      "term" : "",
+      "title" : ""
+    })
+  }
+
+  // GET hello
   sayHi() {
     this.http.get<Response>(this.HTTP_URL + 'hello').subscribe(response => {
       this.message = response.message;
     });
   }
 
+  // POST api/hello
+  sayHiWithCreds() { 
+    let request = {
+      "login" : {
+        "username" : this.loginForm.get('username')?.value,
+        "password" : this.loginForm.get('password')?.value
+      },
+      "object" : {
+        "message" : "no data to transmit"
+      }
+    }
 
-  sayHiWithCreds() {
-    let loginRequest: LoginRequest = {
-      "username" : this.loginForm.get('username')?.value,
-      "password" : this.loginForm.get('password')?.value
-    };
-  
     try {
-      this.http.post<Response>(this.HTTP_URL + 'api/hello',loginRequest).subscribe(response => {
+      this.http.post<Response>(this.HTTP_URL + 'api/hello',request).subscribe(response => {
         this.message = response.message;
       });
     } catch(e) {
@@ -50,24 +64,98 @@ export class ResthttpComponent {
     // })
   }
 
-  
+  // POST user/signup
   registerUser() {
-    let loginRequest: LoginRequest = {
-      "username" : this.loginForm.get('username')?.value,
-      "password" : this.loginForm.get('password')?.value
-    };
+    let request = {
+      "login" : {
+        "username" : this.loginForm.get('username')?.value,
+        "password" : this.loginForm.get('password')?.value
+      }
+    }
 
     try {
-      this.http.post<Response>(this.HTTP_URL + 'user/signup',loginRequest).subscribe(response => {
+      this.http.post<Response>(this.HTTP_URL + 'user/signup',request).subscribe(response => {
         this.message = response.message;
       });
     } catch(e) {
       this.custErrorHandler(e);
     }  
   }
+  
+  changePassword() {
+    let request = {
+      "login" : {
+        "username" : this.loginForm.get('username')?.value,
+        "password" : this.loginForm.get('password')?.value
+      },
+      "new_password" : this.loginForm.get('new_password')?.value 
+    }
+
+    try {
+      this.http.put<Response>(this.HTTP_URL + 'user/changepassword',request).subscribe(response => {
+        this.message = response.message;
+      });
+    } catch(e) {
+      this.custErrorHandler(e);
+    }  
+    
+  }
+
+  addNewStudent() {
+    let request = {
+      "login" : {
+        "username" : this.loginForm.get('username')?.value,
+        "password" : this.loginForm.get('password')?.value
+      },
+      "new_student_nickname" : this.studentForm.get('nickname')?.value 
+    }
+
+    try {
+      this.http.post<Response>(this.HTTP_URL + 'api/student/add',request).subscribe(response => {
+        this.message = response.message;
+      });
+    } catch(e) {
+      this.custErrorHandler(e);
+    }   
+  }
 
   custErrorHandler(e: any) {
     this.message = e.message;
     console.log(e);
   }
+
+  getStudentInfo() {
+    let request = {
+      "login" : {
+        "username" : this.loginForm.get('username')?.value,
+        "password" : this.loginForm.get('password')?.value
+      },
+      "nickname" : this.studentForm.get('nickname')?.value 
+    } 
+  }
+
+  // updateStudentInfo() {
+  //   let request = {
+  //     "login" : {
+  //       "username" : this.loginForm.get('username')?.value,
+  //       "password" : this.loginForm.get('password')?.value
+  //     },
+  //     "nickname" : this.studentForm.get('nickname')?.value,
+  //     "name_first" : this.studentForm.get('name_first')?.value,
+  //     "name_last" : this.studentForm.get('name_last')?.value,
+  //     "name_middle" : this.studentForm.get('name_middle')?.value,
+  //     "school_attending" : this.studentForm.get('school_attending')?.value,
+  //   }
+
+  //   try {
+  //     this.http.put<Response>(this.HTTP_URL + 'api/student/updateinfos',request).subscribe(response => {
+  //       this.message = response.message;
+  //     });
+  //   } catch(e) {
+  //     this.custErrorHandler(e);
+  //   }  
+
+  // }
 }
+
+// TODO angular material
