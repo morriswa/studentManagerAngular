d40548d8759ff2c87a7f507aa8f2c1eb5615a049
morriswa <william@morriswa.org>
2022-07-21 11:51:42 -0500

    deployed v1.2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCommonModule } from '@angular/material/core';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -28,12 +27,14 @@ import { StudentComponent } from './student/student.component';
 import { environment as env } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
+  declarations: 
+  [
     AppComponent,
     LoginComponent,
     StudentComponent
   ],
-  imports: [
+  imports:
+  [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
@@ -51,7 +52,8 @@ import { environment as env } from 'src/environments/environment';
     MatMenuModule,
     MatIconModule,
     MatListModule,
-    AuthModule.forRoot({
+    AuthModule.forRoot(
+    {
       domain: env.auth.domain,
       clientId: env.auth.clientId,
 
@@ -62,25 +64,22 @@ import { environment as env } from 'src/environments/environment';
       scope: 'read:students write:students',
 
       // Specify configuration for the interceptor              
-      httpInterceptor: {
-        allowedList: [
-          {
-            // Match any request that starts 'https://YOUR_DOMAIN/api/v2/' (note the asterisk)
-            uri: env.auth.audience + '*',
-            tokenOptions: {
-              // The attached token should target this audience
-              audience: env.auth.audience,
+      httpInterceptor: { allowedList: [
+        {
+          // Match any request that starts 'https://YOUR_DOMAIN/api/v2/' (note the asterisk)
+          uri: env.auth.audience + '*',
+          tokenOptions: {
+            // The attached token should target this audience
+            audience: env.auth.audience,
 
-              // The attached token should have these scopes
-              scope: 'read:students write:students'
-            }
+            // The attached token should have these scopes
+            scope: 'read:students write:students'
           }
-        ]
-      }
-        })
-      ],
-      providers: [HttpService,
-        { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true }],
-      bootstrap: [AppComponent]
+        }
+      ]}
+    })
+  ],
+  providers: [HttpService,{provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true }],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
